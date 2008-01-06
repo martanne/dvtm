abcc6fd5867849bf9491ad4f56c3f6cb059b0065
Marc Andre Tanner <mat@brain-dump.org>
2008-01-06 22:55:55 +0100

    Document some curses constants.

--- config.h
+++ config.h
@@ -1,4 +1,26 @@
 /* curses attributes for the currently focused window */
+/* valid curses attributes are listed below they can be ORed
+ *
+ * A_NORMAL        Normal display (no highlight)
+ * A_STANDOUT      Best highlighting mode of the terminal.
+ * A_UNDERLINE     Underlining
+ * A_REVERSE       Reverse video
+ * A_BLINK         Blinking
+ * A_DIM           Half bright
+ * A_BOLD          Extra bright or bold
+ * A_PROTECT       Protected mode
+ * A_INVIS         Invisible or blank mode
+ * COLOR(fg,bg)    Color where fg and bg are one of:
+ *
+ *   COLOR_BLACK
+ *   COLOR_RED
+ *   COLOR_GREEN
+ *   COLOR_YELLOW
+ *   COLOR_BLUE
+ *   COLOR_MAGENTA
+ *   COLOR_CYAN
+ *   COLOR_WHITE
+ */
 #define ATTR_SELECTED   COLOR(COLOR_RED,COLOR_BLACK)
 /* curses attributes for normal (not selected) windows */
 #define ATTR_NORMAL     A_NORMAL
@@ -60,6 +82,35 @@ Key keys[] = {
 	{ MOD, '8', focusn, "8" },
 	{ MOD, '9', focusn, "9" },
 };
+
+/* possible values for the mouse buttons are listed below:
+ *
+ * BUTTON1_PRESSED          mouse button 1 down
+ * BUTTON1_RELEASED         mouse button 1 up
+ * BUTTON1_CLICKED          mouse button 1 clicked
+ * BUTTON1_DOUBLE_CLICKED   mouse button 1 double clicked
+ * BUTTON1_TRIPLE_CLICKED   mouse button 1 triple clicked
+ * BUTTON2_PRESSED          mouse button 2 down
+ * BUTTON2_RELEASED         mouse button 2 up
+ * BUTTON2_CLICKED          mouse button 2 clicked
+ * BUTTON2_DOUBLE_CLICKED   mouse button 2 double clicked
+ * BUTTON2_TRIPLE_CLICKED   mouse button 2 triple clicked
+ * BUTTON3_PRESSED          mouse button 3 down
+ * BUTTON3_RELEASED         mouse button 3 up
+ * BUTTON3_CLICKED          mouse button 3 clicked
+ * BUTTON3_DOUBLE_CLICKED   mouse button 3 double clicked
+ * BUTTON3_TRIPLE_CLICKED   mouse button 3 triple clicked
+ * BUTTON4_PRESSED          mouse button 4 down
+ * BUTTON4_RELEASED         mouse button 4 up
+ * BUTTON4_CLICKED          mouse button 4 clicked
+ * BUTTON4_DOUBLE_CLICKED   mouse button 4 double clicked
+ * BUTTON4_TRIPLE_CLICKED   mouse button 4 triple clicked
+ * BUTTON_SHIFT             shift was down during button state change
+ * BUTTON_CTRL              control was down during button state change
+ * BUTTON_ALT               alt was down during button state change
+ * ALL_MOUSE_EVENTS         report all button state changes
+ * REPORT_MOUSE_POSITION    report mouse movement
+ */
 
 Button buttons[] = {
 	{ BUTTON1_CLICKED, mouse_focus, NULL },
