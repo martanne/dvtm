46600bbc40034bdb8afbab60cffd5c240302c98d
Marc Andre Tanner <mat@brain-dump.org>
2008-02-02 14:18:08 +0100

    Fix a bug with minimized clients in tile layout.

--- tile.c
+++ tile.c
@@ -20,29 +20,28 @@ tile(void) {
 			nw = mw;
 			nh = (n - 1 > m) ? wah : wah - m;
 		} else {  /* tile window */
-			if(i == 1) {
-				if(n - 1 > m){
+			if(!c->minimized){
+				if(i == 1) {
 					ny = way;
 					nx += mw;
 					nw = waw - mw;
-				} else
-					ny = way + wah - m;
-				if(!c->minimized){
 					mvvline(ny, nx, ACS_VLINE, wah);
 					mvaddch(ny, nx, ACS_TTEE);
 					nx++, nw--;
 				}
-			} else if(!c->minimized || nm > 1)
-				mvaddch(ny, nx - 1, ACS_LTEE);
-			if(m == 0 && i + 1 == n) /* no minimized clients */
-				nh = (way + wah) - ny;
-			else if(i == nm - 1) /* last not minimized client */
-				nh = (way + wah - (n - i - 1)) - ny;
-			else
-				nh = th;
-
-			if(c->minimized)
+				/* remainder */
+				if(m == 0 && i + 1 == n) /* no minimized clients */
+					nh = (way + wah) - ny;
+				else if(i == nm - 1) /* last not minimized client */
+					nh = (way + wah - (n - i - 1)) - ny;
+				else
+					nh = th;
+			} else {
 				nh = 1;
+				ny = way + wah - (n - i);
+			}
+			if(i > 1 && nm > 1)
+				mvaddch(ny, nx - 1, ACS_LTEE);
 		}
 		resize(c,nx,ny,nw,nh);
 		if(n > 1 && th != wah)
