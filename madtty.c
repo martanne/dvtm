9ad2eabab404f08c7033cffb80806910caa49c39
Marc Andre Tanner <mat@brain-dump.org>
2011-06-12 14:08:09 +0200

    Relicense madtty under ISC license
    I got written permission to do so from:
    
     Bruno T. C. de Oliveira
     Pierre Habouzit
     Ross Palmer Mohn
     Donald Chai
     Niki Yoshiuchi
    
    Signed-off-by: Marc Andre Tanner <mat@brain-dump.org>

--- madtty.c
+++ madtty.c
@@ -1,23 +1,19 @@
 /*
-    LICENSE INFORMATION:
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License (LGPL) as published by the Free Software Foundation.
-
-    Please refer to the COPYING file for more information.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
-
-    Copyright © 2004 Bruno T. C. de Oliveira
-    Copyright © 2006 Pierre Habouzit
-    Copyright © 2008-2011 Marc Andre Tanner
+ * Copyright © 2004 Bruno T. C. de Oliveira
+ * Copyright © 2006 Pierre Habouzit
+ * Copyright © 2008-2011 Marc Andre Tanner
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF MIND, USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #define _GNU_SOURCE
