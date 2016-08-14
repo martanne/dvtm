518725cebfe321e7558f439336876b0d2b69de6a
Ross Mohn <rpmohn@waxandwane.org>
2016-08-14 17:19:00 -0400

    First commit of previous mousetag patch

--- config.def.h
+++ config.def.h
@@ -185,6 +185,13 @@ static Button buttons[] = {
 	{ BUTTON2_CLICKED,        { mouse_zoom,       { NULL  } } },
 	{ BUTTON3_CLICKED,        { mouse_minimize,   { NULL  } } },
 };
+
+static Button tag_buttons[] = {
+	{ BUTTON1_CLICKED,        { mouse_toggleview, { NULL  } } },
+	{ BUTTON1_DOUBLE_CLICKED, { mouse_view,       { NULL  } } },
+	{ BUTTON2_CLICKED,        { view,             { NULL  } } },
+	{ BUTTON3_CLICKED,        { mouse_toggletag,  { NULL  } } },
+};
 #endif /* CONFIG_MOUSE */
 
 static Cmd commands[] = {
